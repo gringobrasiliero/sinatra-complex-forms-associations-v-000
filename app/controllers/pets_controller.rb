580c3a71537885608e8b8fc9d1e3e72d06a5233f
Nolan Byrnes <nolanehs@gmail.com>
2018-01-24 11:52:48 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,25 +1,25 @@
 class PetsController < ApplicationController
 
   get '/pets' do
-    @pets = Pet.all
-    erb :'/pets/index' 
+    @owners = Owner.all
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
     erb :'/pets/new'
   end
 
-  post '/pets' do 
+  post '/pets' do
 
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  post '/pets/:id' do 
+  post '/pets/:id' do
 
     redirect to "pets/#{@pet.id}"
   end
