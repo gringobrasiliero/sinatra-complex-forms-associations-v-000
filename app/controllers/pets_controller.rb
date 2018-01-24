657a82d3fdd78abc3ef33e52d4dc1da78d37938e
Nolan Byrnes <nolanehs@gmail.com>
2018-01-24 12:42:24 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,11 +6,11 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do
-      @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do
+
     @pet = Pet.create(params[:pet])
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
@@ -31,7 +31,12 @@ class PetsController < ApplicationController
   end
 
   post '/pets/:id' do
-
+    @pet = Pet.find(params[:id])
+        @pet.update(params[:pet])
+        if !params["owner"]["name"].empty?
+          @pet.owner = Owner.create(name: params["owner"]["name"])
+        end
+        @pet.save
     redirect to "pets/#{@pet.id}"
   end
 end
